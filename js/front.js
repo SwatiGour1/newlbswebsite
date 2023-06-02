0fb783feb25bd3c264bfa2ceef47537199879538
swati <iamswatigour@gmail.com>
2023-06-02 08:16:57 -0700

    add pages

--- js/front.js
+++ js/front.js
@@ -20,3 +20,39 @@ updatecounter();
 function startBlinkAnimation(element) {
 element.classList.add("blink-animation");
 }
+function initMap() {
+    // Specify your latitude and longitude coordinates
+    var myLatLng = {lat: 37.7749, lng: -122.4194};
+
+    // Create a map centered on the specified coordinates
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: myLatLng,
+      zoom: 12
+      
+    });
+
+    // Add a marker at the specified coordinates
+    var marker = new google.maps.Marker({
+      map: map,
+      position: myLatLng,
+      title: 'Our Location'
+    });
+  }
+
+
+
+  window.addEventListener('scroll', checkScroll);
+
+function checkScroll() {
+const imageElements = document.querySelectorAll('.image-grid img');
+
+for (let i = 0; i < imageElements.length; i++) {
+  const imageElement = imageElements[i];
+  const imagePosition = imageElement.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+  
+  if (imagePosition < windowHeight) {
+    imageElement.classList.add('animate');
+  }
+}
+}
