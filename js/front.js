e26c838dbc5570afe48fbe0c31fdb9cbb0f25b2a
swati <iamswatigour@gmail.com>
2023-06-04 21:20:55 -0700

    arrange pages

--- js/front.js
+++ js/front.js
@@ -56,142 +56,7 @@ for (let i = 0; i < imageElements.length; i++) {
   }
 }
 }
-// biometric
-$(document).ready(function() {
-  // Student reports array
-  var studentReports = [];
 
-  // Handle form submission
-  $('#reportForm').submit(function(event) {
-    event.preventDefault();
-
-    // Get form values
-    var studentName = $('#studentName').val();
-    var department = $('#department').val();
-    var shift = $('#shift').val();
-    var inTime = $('#inTime').val();
-    var outTime = $('#outTime').val();
-    var totalTime = $('#totalTime').val();
-    var attendance = $('#attendance').val();
-
-    // Add the report to the array
-    studentReports.push({
-      name: studentName,
-      department: department,
-      shift: shift,
-      inTime: inTime,
-      outTime: outTime,
-      totalTime: totalTime,
-      attendance: attendance
-    });
-
-    // Append a new row to the table
-    $('#reportTable tbody').append(
-      '<tr>' +
-      '<td><input type="checkbox" class="report-checkbox" data-name="' + studentName + '"></td>' +
-      '<td>' + studentName + '</td>' +
-      '<td>' + department + '</td>' +
-      '<td>' + shift + '</td>' +
-      '<td>' + inTime + '</td>' +
-      '<td>' + outTime + '</td>' +
-      '<td>' + totalTime + '</td>' +
-      '<td>' + attendance + '</td>' +
-      '</tr>'
-    );
-
-    // Reset form fields
-    $('#studentName').val('');
-    $('#department').val('');
-    $('#shift').val('');
-    $('#inTime').val('');
-    $('#outTime').val('');
-    $('#totalTime').val('');
-    $('#attendance').val('');
-  });
-
-  // Export as PDF
-  $('#exportPdfBtn').click(function() {
-    var selectedReports = getSelectedReports();
-
-    if (selectedReports.length === 0) {
-      alert('No reports selected.');
-      return;
-    }
-
-    var doc = new jsPDF();
-    doc.setFontSize(18);
-    doc.text('Student Daily Reports', 10, 10);
-    doc.setFontSize(12);
-
-    var y = 20;
-    $.each(selectedReports, function(index, report) {
-      doc.text(10, y + 10, 'Student Name: ' + report.name);
-      doc.text(10, y + 20, 'Department: ' + report.department);
-      doc.text(10, y + 30, 'Shift: ' + report.shift);
-      doc.text(10, y + 40, 'In Time: ' + report.inTime);
-      doc.text(10, y + 50, 'Out Time: ' + report.outTime);
-      doc.text(10, y + 60, 'Total Time: ' + report.totalTime);
-      doc.text(10, y + 70, 'Attendance: ' + report.attendance);
-      y += 90;
-    });
-
-    doc.save('student_reports.pdf');
-  });
-
-  // Export as Excel
-  $('#exportExcelBtn').click(function() {
-    var selectedReports = getSelectedReports();
-
-    if (selectedReports.length === 0) {
-      alert('No reports selected.');
-      return;
-    }
-
-    var worksheet = XLSX.utils.json_to_sheet(selectedReports);
-    var workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, 'Student Reports');
-    XLSX.writeFile(workbook, 'student_reports.xlsx');
-  });
-
-  // Clear Selected Reports
-  $('#clearReportsBtn').click(function() {
-    var selectedReports = getSelectedReports();
-
-    if (selectedReports.length === 0) {
-      alert('No reports selected.');
-      return;
-    }
-
-    $.each(selectedReports, function(index, report) {
-      // Remove the report from the array
-      studentReports = studentReports.filter(function(item) {
-        return item.name !== report.name;
-      });
-
-      // Remove the row from the table
-      $('input.report-checkbox[data-name="' + report.name + '"]').closest('tr').remove();
-    });
-  });
-
-  // Get the selected reports
-  function getSelectedReports() {
-    var selectedReports = [];
-
-    $('input.report-checkbox:checked').each(function() {
-      var name = $(this).data('name');
-
-      var report = studentReports.find(function(item) {
-        return item.name === name;
-      });
-
-      if (report) {
-        selectedReports.push(report);
-      }
-    });
-
-    return selectedReports;
-  }
-});
 // result
 function myfunction() {
   alert("Results is opening  please wait a second!");
@@ -199,3 +64,14 @@ function myfunction() {
 function MyClose(){
 confirm("do you want to close this");
 }
+// var p=document.querySelector(".popbox");
+// function pop(){
+//  setTimeout(
+//     function(){
+//      p.style.display="block";
+//   },1000)
+// }
+// let f=document.querySelector(".popbox")
+// function close(){
+// f.style.display='none';
+// }
